ee83b861afc515d3c9d335630c714a08f1557b9e
lucas <diasb.trabalho@gmail.com>
2020-09-22 12:09:33 -0300

    Resolução de exercícios em python

--- index.py
+++ index.py
@@ -1,4 +1,44 @@
+#Resoluçao 1001
+
+'''
 a=int(input('Digite um número: '))
 b=int(input('Digite um numero: '))
 x=a+b
 print(f'A soma dos valores é igual à {x}')
+
+'''
+
+#Resolução 1002
+
+'''
+n=3.14159
+raio=float(input())
+A=(n*(raio*raio))
+print(f'A={A:.4f}')
+
+'''
+
+#Resolução 1003
+
+'''
+A=int(input())
+B=int(input())
+S=A+B
+print(f'SOMA = {S}')
+'''
+
+#Resolução 1004
+
+'''
+A=int(input())
+B=int(input())
+PROD=A*B
+print(f'PROD = {PROD}')
+'''
+
+#Resolução 1005
+
+A=float(input())
+B=float(input())
+MEDIA=(A*3.5+B*7.5)/11
+print(f'MEDIA = {MEDIA}')
